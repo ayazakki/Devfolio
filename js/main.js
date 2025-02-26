4561a85eadf75d7b2c1683c5ede8661246a5bb9e
ayazakki <ayai42607@gmail.com>
2025-02-27 00:50:20 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Designer', 'Developer' , 'Freelancer' , 'Photographer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+});
